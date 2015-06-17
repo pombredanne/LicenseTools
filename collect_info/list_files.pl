5fda46a1f48db8a0bac38214786f5f096197c281
Wu Yuhao <wyhfrank@gmail.com>
2015-06-17 15:23:02 +0900

    Update list_file.pl

--- collect_info/list_files.pl
+++ collect_info/list_files.pl
@@ -8,6 +8,7 @@ use strict;
 
 my $src_root = @ARGV[0];
 my $stat_root = @ARGV[1];
+my $ext = @ARGV[2];
 # my $startPkgName = @ARGV[0];
 
 
@@ -25,63 +26,34 @@ my $dbh = DBI->connect($dsn, $userid, $password, { AutoCommit => 0, RaiseError =
 
 
 my $stmt = qq(CREATE TABLE IF NOT EXISTS FILE
-       (PKGNAME       TEXT    NOT NULL,
-       PATH       TEXT    NOT NULL,
-       BASENAME       TEXT    NOT NULL,
-       EXT       TEXT););
+       (PATH       TEXT    NOT NULL,
+       HASH       TEXT););
 my $rv = $dbh->do($stmt);
 if($rv < 0){
    print $DBI::errstr;
 }
 
-my $sth = $dbh->prepare('INSERT INTO FILE (PKGNAME, PATH, BASENAME, EXT) VALUES (?, ?, ?, ?)');
+my $sth = $dbh->prepare('INSERT INTO FILE (PATH) VALUES (?)');
 
 
-my @pkgs=`find $src_root -mindepth 1 -maxdepth 1 -type d`;
+print "Making .$ext file list...\n";
 
-
-# if ($startPkgName) {
-#  my $index = first_index{/$startPkgName$/} @pkgs;
-#  if ($index > 0) {
-#   splice @pkgs, 0, $index;
-#  }
-# }
-
-my $pkgCount = @pkgs;
-print "Package Number: $pkgCount\n";
-
-my $count = 1;
-foreach my $pkg (@pkgs) {
+my @files=`find $src_root -name '*.$ext' -type f`;
 
  my $startTime=time();
- 
- chomp $pkg;
- my $pkg_name = basename($pkg);
- print "Parsing pkg [$count of $pkgCount]: $pkg_name ... "; 
+foreach my $file (@files) {
 
- 
-# print "find $pkg -type f\n";
+  chomp $file;
 
- my @files=`find $pkg -type f`;
-# print "Files: @files \n";
-
- foreach my $file (@files) {
-  
-  (my $name,my $path,my $suffix) = fileparse($file,qr"\..[^.]*$");
-  chomp $name;
-  chomp $path;
-  chomp $suffix;
-
-  # print "Inserting ($pkg_name, $path, $name, $suffix)\n";
-  my @values=($pkg_name, $path, $name, $suffix);
+  # print "Inserting ($file)\n";
+  my @values=($file);
   $sth->execute(@values);
- }
+
+}
  $dbh->commit;
- $count++;
 
  my $timePassed = time() - $startTime;
  print "[${timePassed}s used]\n";
-}
 
 $dbh->disconnect();
 
