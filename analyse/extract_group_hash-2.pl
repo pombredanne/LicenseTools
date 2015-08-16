5a5446a05ce7d620dc3e164c435396ee9c03c177
Wu Yuhao <wyhfrank@gmail.com>
2015-08-16 15:32:58 +0900

    Update hash extractor.

--- analyse/extract_group_hash-2.pl
+++ analyse/extract_group_hash-2.pl
@@ -9,76 +9,81 @@ use Getopt::Std;
 use Cwd;
 
 
-my $dir_root = "AnalysisData-Debian-new/";
+my $dir_root = "AnalysisData/";
 
 chomp(my @subs = `find $dir_root -mindepth 1 -maxdepth 1 -type d`);
 
 foreach my $sub (@subs) {
 
-print "Processing [$sub]...\n";
+	print "Processing [$sub]...\n";
 
-my $src_root = "$sub/Source/";
-my $stat_root = "$sub/Statistics/";
-my $inconList = "${stat_root}LicenseChanged.csv";
-my $inconListSorted = "${stat_root}LicenseChanged.sorted.csv";
-my $inconListHash = "${stat_root}LicenseChanged.hash.csv";
-my $hashTable = "${stat_root}hash_map.txt";
-
-
-if (!-e $inconListSorted) {
-  `cat $inconList | sort -n > $inconListSorted`;
-}
-
-my $lastProcessedGroup=-1;
-
-if (-e $inconListHash) {
-  chomp(my $lastLine=`tail -n 1 $inconListHash`);
-  (my $hash, my $r) = split(/,/,$lastLine);
-  if ($r) {
-    $lastProcessedGroup = $r;
-  }
-}
-
-print "lastProcessedGroup: $lastProcessedGroup\n";
-
-my $r=`wc -l $inconListSorted`;
-(my $totalGroups)=split(/ /, $r);
-
-open(my $inconFH, "<$inconListSorted") or die$!;
-open(my $hashFH, ">>$inconListHash");
-
-my $count=1;
-while(<$inconFH>) {
-
-chomp;
-(my $groupNum) = split(/,/);
-
-if ($groupNum <= $lastProcessedGroup) {
-print "Skip $groupNum\r";
-$count++;
-next;
-}
-
-print "Processing $groupNum [$count/$totalGroups]\n";
-
-my $map="${src_root}$groupNum/mapping.txt";
-chomp(my $line=`head -n 1 $map`);
-(my $name, my $file)=split(/,/,$line);
-
-#print "First line of map is : $line\nFile is: $file\n";
-
-$line=`grep '$file' $hashTable`;
-my $hash;
-if ($line) {
-($hash)=split(/,/,$line);
-#print "hash is:[$hash]\n";
-}
-print $hashFH "$hash,$_\n";
-
-$count++;
-}
+	my $src_root = "$sub/Source/";
+	my $stat_root = "$sub/Statistics/";
+	my $inconList = "${stat_root}LicenseChanged.csv";
+	my $inconListSorted = "${stat_root}LicenseChanged.sorted.csv";
+	my $inconListHash = "${stat_root}LicenseChanged.hash.csv";
+	my $errList = "${stat_root}LicenseChanged.error.csv";
+	my $hashTable = "${stat_root}hash_map.txt";
 
 
-close($inconFH);
-close($hashFH);
+	if (!-e $inconListSorted) {
+	  print "Sorting $inconList...\n";
+	  `cat $inconList | sort -n > $inconListSorted`;
+	}
+
+	my $lastProcessedGroup=-1;
+
+	if (-e $inconListHash) {
+	  chomp(my $lastLine=`tail -n 1 $inconListHash`);
+	  (my $hash, my $r) = split(/,/,$lastLine);
+	  if ($r) {
+	    $lastProcessedGroup = $r;
+	  }
+	}
+
+	print "lastProcessedGroup: $lastProcessedGroup\n";
+
+	chomp(my $totalGroups=`wc -l < '$inconListSorted'`);
+
+	open(my $inconFH, "<$inconListSorted") or die$!;
+	open(my $hashFH, ">>$inconListHash");
+
+	my $count=1;
+	while(<$inconFH>) {
+
+		chomp;
+		(my $groupNum) = split(/,/);
+
+		if ($groupNum <= $lastProcessedGroup) {
+			print "Skip $groupNum\r";
+			$count++;
+			next;
+		}
+
+		print "Processing $groupNum [$count/$totalGroups]\n";
+
+		my $list="${src_root}$groupNum/list.txt";
+		my $hash;
+
+		if (-e $list) {
+			chomp(my $file=`head -n 1 '$list'`);
+			my @r=`grep '$file,' $hashTable` if ($file);
+			my $num=@r;
+			if ($num==1) {
+				my $line=$r[0];
+				($hash)=split(/,/,$line) if ($line);
+			} else {
+				# exception.
+				`echo '$list,$file\n' >> $errList`;
+			}
+		} else {
+			`echo '$list,\n' >> $errList`;
+		}
+
+		print $hashFH "$hash,$_\n";
+		$count++;
+	}
+
+	close($inconFH);
+	close($hashFH);
 }
