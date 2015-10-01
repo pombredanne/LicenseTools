722672671413628de3710ff4ed68ba1c616b8544
Wu Yuhao <wyhfrank@gmail.com>
2015-10-01 16:50:23 +0900

    Update calc_file_num_per_group.pl

--- analyse/calc_file_num_per_group.pl
+++ analyse/calc_file_num_per_group.pl
@@ -10,13 +10,10 @@ use Cwd;
 
 
 my $dir_root = "AnalysisData/";
-my $dist = "${dir_root}FileNumPerGroup.txt";
+my $dist = "${dir_root}FileNumPerGroup.csv";
 
 chomp(my @subs = `find $dir_root -mindepth 1 -maxdepth 1 -type d`);
 
-my $groupCountT=0;
-my $fileCountT=0;
-my $maxfileCountT=0;
 my $txt;
 
 foreach my $sub (@subs) {
@@ -24,39 +21,23 @@ foreach my $sub (@subs) {
 	print "Processing [$sub]...\n";
 
 	my $stat_root = "$sub/Statistics/";
-	my $distri = "${stat_root}FileNumPerGroup.txt";
 	my $srcUniqList = "${stat_root}SrcUniqList.txt";
 
-	my $groupCount=0;
-	my $fileCount=0;
-	my $maxfileCount=0;
 	open(FILE, "<$srcUniqList") or die $!;
 	while(<FILE>) {
 		chomp;
 
 		my $list=$_ . "/list.txt";
 		chomp(my $count=`wc -l < '$list'`);
-		$fileCount += $count;
-		$maxfileCount = $count>$maxfileCount ? $count : $maxfileCount;
+		$txt .= "$sub,$_,$count\n";
 
-		$groupCount++;
 	}
 	close(FILE);
 
-	$groupCountT+=$groupCount;
-	$fileCountT+=$fileCount;
-	$maxfileCountT = $maxfileCount>$maxfileCountT ? $maxfileCount : $maxfileCountT;
-	
-	my $avg=$fileCount/$groupCount;
 
-#	open(FILE, ">$distri") or die $!;
-#	print FILE "$sub\nGroupTotal: $groupCount\nFileCount: $fileCount\nMaxFileCount: $maxfileCount\nAvg: $avg\n";
-#	close(FILE);
 #	exit;
-$txt.="$sub\nGroupTotal: $groupCount\nFileCount: $fileCount\nMaxFileCount: $maxfileCount\nAvg: $avg\n";
 }
 
-my $avgT=$fileCountT/$groupCountT;
 open(FILE, ">$dist") or die $!;
-print FILE "GroupTotal: $groupCountT\nFileCount: $fileCountT\nMaxFileCount: $maxfileCountT\nAvg: $avgT\n\n$txt";
+print FILE $txt; 
 close(FILE);
